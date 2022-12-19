662568fd1ac21807296cc8c32f6ceb1f62df4412
Harshil Gupta <harshilgupta1808@gmail.com>
2022-12-19 21:11:48 +0530

    adding type checking and humanised responses

--- main.py
+++ main.py
@@ -12,7 +12,12 @@ db_op.init_db(conn)
 
 @app.post('/customer')
 @token_required
-def add_customer():
+def add_customer() -> dict:
+    """
+    Adds a customer to the database
+    Automatically creates a country if not available
+    :return:
+    """
     payload = request.get_json()
     emp = create_emp_obj(payload)
     emp.dept_id = int(db_op.get_department_id(conn, emp.department))
@@ -21,42 +26,64 @@ def add_customer():
             INSERT INTO customer(id,name,phone,email,Dept_id,Country_id) VALUES ('%s','%s',%d,'%s',%d,%d) RETURNING id
           ''' % (emp.ID, emp.name, emp.phone, emp.email, emp.dept_id, emp.country_id)
     resp = db_op.execute_with_get(conn, sql)
-    return f"Customer added with ID {resp[0]}"
+    return {"message": f"Customer added with ID {resp[0]}"}
+
 
 
 @app.get("/customer/all")
 @token_required
-def get_all_customer():
+def get_all_customer() -> dict:
+    """
+    Gets all the customers
+    :return:
+    """
     emp_list = db_op.get_all_customers_from_db(conn)
     return emp_list
 
 
 @app.get("/customer/<id>")
 @token_required
-def get_customer(id):
+def get_customer(id: str) -> dict:
+    """
+    Gets a customer detail by id
+    :param id:
+    :return:
+    """
     emp = db_op.get_customer_from_db(conn, id)
+    if not emp:
+        return {"message": f"Not a valid id {id}"}
     return emp.to_json()
 
 
 @app.delete("/customer/<id>")
 @token_required
-def delete_customer(id):
+def delete_customer(id: str) -> dict:
+    """
+    Deletes a customer
+    :param id:
+    :return:
+    """
     sql = '''
         DELETE FROM customer
         where id='%s' RETURNING id ;
     ''' % id
     print(sql)
     resp = db_op.execute_with_get(conn, sql)
-    return "Success"
+    return {"message": f"deleted {id}"}
 
 
 @app.post("/customer/<id>")
 @token_required
-def update_customer(id):
+def update_customer(id: str) -> dict:
+    """
+    Updates a customer
+    :param id:
+    :return:
+    """
     payload = request.get_json()
     existing_details = db_op.get_customer_from_db(conn, id)
     if not existing_details:
-        return f"Not a valid id {id}"
+        return {"message": f"Not a valid id {id}"}
     if "department" in payload:
         d_id = db_op.get_department_id(conn, payload["department"])
         existing_details.dept_id = d_id
@@ -74,7 +101,7 @@ def update_customer(id):
            existing_details.country_id, id)
     print(sql)
     resp = db_op.execute_with_get(conn, sql)
-    return "Success"
+    return {"message": f"updated {id}"}
 
 
 app.run(debug=True, host='127.0.0.1', port=8080)
